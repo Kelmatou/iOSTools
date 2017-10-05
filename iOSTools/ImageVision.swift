ee37fbef4296a4b181b6ca04629f39d86fd70b5b
Antoine Clop <antoine.clop@3ie.fr>
2017-10-05 11:53:29 +0200

    [FIX] recognition results are not returned as a list and not dictionary

--- iOSTools/ImageVision.swift
+++ iOSTools/ImageVision.swift
@@ -25,10 +25,10 @@ public class ImageVision {
    - parameter image: the picture to analyse
    - parameter maxResult: the maximum number of result returned
    
-   - returns: a dictionary of possible model objects and the prediction accuracy [ "modelObject" : probability ] (nil is return is case of trouble)
+   - returns: a list of possible model objects and the prediction accuracy [ ("modelObject" , probability) ] (nil is return is case of trouble)
               This probability is expressed as percentage
    */
-  public func detect(in image: UIImage, maxResult: Int? = nil, completion: @escaping ([String : Float]?, GenericError?) -> Void) {
+  public func detect(in image: UIImage, maxResult: Int? = nil, completion: @escaping ([(String, Float)]?, GenericError?) -> Void) {
     guard let ciImage = CIImage(image: image) else {
       debugPrint("[ERROR]: Failed to convert image to CIImage")
       completion(nil, ImageError("Failed to convert image to CIImage"))
@@ -48,14 +48,14 @@ public class ImageVision {
         return
       }
       
-      var resultsDictionnary: [String : Float] = [:]
+      var resultsList: [(String, Float)] = []
       for result in results {
-        resultsDictionnary[result.identifier] = result.confidence * 100
-        if resultsDictionnary.count == maxResult {
+        resultsList.append((result.identifier, result.confidence * 100))
+        if resultsList.count == maxResult {
           break
         }
       }
-      completion(resultsDictionnary, nil)
+      completion(resultsList, nil)
     }
     
     let handler = VNImageRequestHandler(ciImage: ciImage)
@@ -76,10 +76,10 @@ public class ImageVision {
    - parameter model: the model that represents objects to find
    - parameter maxResult: the maximum number of result returned
    
-   - returns: a dictionary of possible model objects and the prediction accuracy [ "modelObject" : probability ] (nil is return is case of trouble)
-   This probability is expressed as percentage
+   - returns: a list of possible model objects and the prediction accuracy [ ("modelObject" , probability) ] (nil is return is case of trouble)
+              This probability is expressed as percentage
    */
-  public static func detectModel(in image: UIImage, model: MLModel, maxResult: Int? = nil, completion: @escaping ([String : Float]?, GenericError?) -> Void) {
+  public static func detectModel(in image: UIImage, model: MLModel, maxResult: Int? = nil, completion: @escaping ([(String, Float)]?, GenericError?) -> Void) {
     let imageVisionObject: ImageVision = ImageVision(model)
     imageVisionObject.detect(in: image, maxResult: maxResult) { (result, error) in
       completion(result, error)
@@ -92,10 +92,10 @@ public class ImageVision {
    - parameter image: the picture to analyse
    - parameter maxResult: the maximum number of result returned
    
-   - returns: a dictionary of possible objects and the prediction accuracy [ "object" : probability ] (nil is return is case of trouble)
+   - returns: a list of possible objects and the prediction accuracy [ ("object" , probability) ] (nil is return is case of trouble)
               This probability is expressed as percentage
    */
-  public static func detectObject(in image: UIImage, maxResult: Int? = nil, completion: @escaping ([String : Float]?, GenericError?) -> Void) {
+  public static func detectObject(in image: UIImage, maxResult: Int? = nil, completion: @escaping ([(String, Float)]?, GenericError?) -> Void) {
     let imageVisionObject: ImageVision = ImageVision(MobileNet().model)
     imageVisionObject.detect(in: image, maxResult: maxResult) { (result, error) in
       completion(result, error)
@@ -108,10 +108,10 @@ public class ImageVision {
    - parameter image: the picture to analyse
    - parameter maxResult: the maximum number of result returned
    
-   - returns: a dictionary of possible room type and the prediction accuracy [ "room" : probability ] (nil is return is case of trouble)
+   - returns: a list of possible rooms type and the prediction accuracy [ ("place" , probability) ] (nil is return is case of trouble)
               This probability is expressed as percentage
    */
-  public static func detectPlaces(in image: UIImage, maxResult: Int? = nil, completion: @escaping ([String : Float]?, GenericError?) -> Void) {
+  public static func detectPlaces(in image: UIImage, maxResult: Int? = nil, completion: @escaping ([(String, Float)]?, GenericError?) -> Void) {
     let imageVisionObject: ImageVision = ImageVision(GoogLeNetPlaces().model)
     imageVisionObject.detect(in: image, maxResult: maxResult) { (result, error) in
       completion(result, error)
